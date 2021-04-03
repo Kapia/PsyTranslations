c46f468a6c6cead5f18f2462014f0c9929e2bd2e
EnriqueQuiros <EnriqueQuiros@users.noreply.github.com>
2021-04-03 09:57:39 +0000

    deploy: d36d73a11fb782d543c563accb317316ca340d85

---
+++ js/app.js
@@ -0,0 +1,1 @@
+import s from"../_snowpack/pkg/turbolinks.js";import"../css/mystyles.css.proxy.js";s.start();const e=document.documentElement,c=t=>{e.dataset.theme=t,localStorage.setItem("theme",t)};
